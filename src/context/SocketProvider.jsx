2760e1fcb06bfc4560d9afc32e3482e33b4d8c1a
Dhruvgos <dhruv197475@gmail.com>
2025-01-03 10:55:54 +0530

    changed url

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -4,12 +4,13 @@ import { io } from 'socket.io-client'; // Make sure to install socket.io-client
 // Create a context for the socket connection
 export const SocketContext = createContext();
 
-const SOCKET_URL = import.meta.env.REACT_APP_S_URL||"http://localhost:4000"; // Your socket server URL
+const SOCKET_URL = import.meta.env.VITE_S_URL||"http://localhost:4000"; // Your socket server URL
 
 export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
+    // console.log(SOCKET_URL)
     const socketInstance = io(SOCKET_URL); // Establish socket connection
     setSocket(socketInstance);
     console.log("connected")
